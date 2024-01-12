b40b9a8aef03c7a91d5be11ac77b278636c644a3
lovebssbds <sweetlina.syk@gmail.com>
2024-01-12 17:45:54 +0900

    hover botton making

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(function(){
-    
+    if($(window).width()>375){
+        if($('.d-links-g').find('.contents').scrollLeft()==0){
+            $('.d-links-g>.contents').hover(
+                function(){
+                    $('.d-links-next').show();
+                },
+                function(){
+                    $('.d-links-next').hide();
+                }
+            )
+        }
+    }
+    console.log('scroll left'+$('.d-links-g').find('.contents').scrollLeft())
 })
