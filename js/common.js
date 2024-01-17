0561d306c7aedf25729346277bc70412cc0124d0
lovebssbds <sweetlina.syk@gmail.com>
2024-01-17 10:01:07 +0900

    add favicon

--- js/common.js
+++ js/common.js
@@ -1,15 +1,11 @@
-$(function(){
-    if($(window).width()>375){
-        if($('.d-links-g').find('.contents').scrollLeft()==0){
-            $('.d-links-g>.contents').hover(
-                function(){
-                    $('.d-links-next').show();
-                },
-                function(){
-                    $('.d-links-next').hide();
-                }
-            )
+$(function(){   
+    $('.d-links-g').hover(
+        function(){
+            $('.d-links-next').css('visibility','visible')
+            
+        },
+        function(){
+            $('.d-links-next').css('visibility','hidden')
         }
-    }
-    console.log('scroll left'+$('.d-links-g').find('.contents').scrollLeft())
+    )
 })
